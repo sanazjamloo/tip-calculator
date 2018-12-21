1c9d71ed9751c3485665ff719268485fd3108560
sanazjamloo <sanaz.jamloo@gmail.com>
2018-12-20 21:06:15 -0800

    add calculation methods

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,8 +1,8 @@
 //Calculate tip
 function calculateTip () {
-  let billAmount = document.getElementById("bill").value;
-  let serviceQual = document.getElementById("serviceQual").value;
-  let numofPeople = document.getElementById("peopleCount").value;
+  var billAmount = document.getElementById("bill").value;
+  var serviceQual = document.getElementById("serviceQual").value;
+  var numOfPeople = document.getElementById("peopleCount").value;
 
   //validate input
   if (billAmount === "" || serviceQual == 0) {
@@ -10,11 +10,30 @@ function calculateTip () {
     return;
   }
   //Check to see if the input is empty or less than or equal to 1
-  if (numofPeople === "" || numofPeople <= 1) {
-    numofPeople = 1;
+  if (numOfPeople === "" || numOfPeople <= 1) {
+    numOfPeople = 1;
     document.getElementById("each").style.display = "none";
   } else {
     document.getElementById("each").style.display = "block";
   }
 
+  //Calculate tip
+  var total = (billAmount * serviceQual) / numOfPeople;
+  //round to two decimal places
+  total = Math.round(total * 100) / 100;
+  //next line allows us to always have two digits after decimal point
+  total = total.toFixed(2);
+  //Display the tip
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
 }
+
+//Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+//click to call function
+document.getElementById("calculate").onclick = function() {
+  calculateTip();
+
+};
