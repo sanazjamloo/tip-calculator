1672a24db490384c1f9f0f093fef9c58e21d640c
sanazjamloo <sanaz.jamloo@gmail.com>
2018-12-20 20:32:47 -0800

    add calculation function

---
+++ tipcalculator.js
@@ -0,0 +1,6 @@
+//Calculate tip
+function calculateTip () {
+  let billAmount = document.getElementById("bill").value;
+  let serviceQual = document.getElementById("serviceQual").value;
+  let numofPeople = document.getElementById("peopleCount").value;
+}
