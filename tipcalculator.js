03cf4da7a3a87aaebf626c56757f025e583e97ee
sanazjamloo <sanaz.jamloo@gmail.com>
2018-12-20 20:41:02 -0800

    add validation methods

--- tipcalculator.js
+++ tipcalculator.js
@@ -3,4 +3,18 @@ function calculateTip () {
   let billAmount = document.getElementById("bill").value;
   let serviceQual = document.getElementById("serviceQual").value;
   let numofPeople = document.getElementById("peopleCount").value;
+
+  //validate input
+  if (billAmount === "" || serviceQual == 0) {
+    alert("Please enter values");
+    return;
+  }
+  //Check to see if the input is empty or less than or equal to 1
+  if (numofPeople === "" || numofPeople <= 1) {
+    numofPeople = 1;
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+
 }
